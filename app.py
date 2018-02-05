5428f545ec22be0d68580e68a2b4f00cbeae419e
JonW27 <18wongj@gmail.com>
2018-02-05 17:16:18 -0500

    add texlive buildpack

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import render_template
+import os
+from flask import Flask, render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('auth.html');
+
+def editor():
+    
+    return render_template('editor.html');
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='127.0.0.1', port=port)
